027fcaad22d23396aa9a780ffaf354d2200d2894
Samuel Kendall <kinguser981@gmail.com>
2024-10-04 03:10:48 +0330

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,6 @@ ALLOW_MISSING_DEPENDENCIES := true
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
-    boot \
     system \
     system_ext \
     product \
@@ -35,22 +34,27 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a9
 
-# APEX
-DEXPREOPT_GENERATE_APEX_IMAGE := false
-TW_EXCLUDE_APEX := true
+# Block
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+
+# Board Properties
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_INCLUDE_RECOVERY_DTBO := true
 
 # Bootloader
-TARGET_NO_BOOTLOADER := true
+TARGET_NO_BOOTLOADER := false
 
 # Build Rules
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
+# Decryption
+BOARD_USES_QCOM_FBE_DECRYPTION := true
+
 # Crypto
 TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO_FBE := true
-TW_INCLUDE_FBE_METADATA_DECRYPT := true
-BOARD_USES_QCOM_FBE_DECRYPTION := true
 
 # Debugging Configs
 TWRP_INCLUDE_LOGCAT := true
@@ -59,19 +63,22 @@ TARGET_USES_LOGD := true
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_IMAGE_NAME := kernel
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x4a90000 loop.max_part=7 cgroup.memory=nokmem,nosocket
 BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_BASE := 0x00000000
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x4a90000 loop.max_part=7 cgroup.memory=nokmem,nosocket
 
+# Kernel - config
 TARGET_KERNEL_CONFIG := cap_sprout_defconfig
 TARGET_KERNEL_SOURCE := kernel/nokia/cap_sprout
 
+# Kernel - prebuilt
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/$(BOARD_KERNEL_IMAGE_NAME)
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 
+# Kernel - prebuilt args
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
@@ -80,37 +87,35 @@ BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
-# Partitions
-BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+# mke2fs
+TARGET_USES_MKE2FS := true
+
+# Partitions - size
 BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 100663296
-BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Partitions - type
 BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
+
+# Partitions - super
 BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
 BOARD_SUPER_PARTITION_GROUPS := nokia_dynamic_partitions
 BOARD_NOKIA_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor
 BOARD_NOKIA_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
 
 # Platform
-BOARD_USES_QCOM_HARDWARE := true
-TARGET_BOARD_PLATFORM := trinket
-TARGET_BOOTLOADER_BOARD_NAME := $(TARGET_BOARD_PLATFORM)
-QCOM_BOARD_PLATFORMS := $(TARGET_BOARD_PLATFORM)
+TARGET_BOARD_PLATFORM := $(PRODUCT_PLATFORM)
+TARGET_BOOTLOADER_BOARD_NAME := $(PRODUCT_PLATFORM)
+QCOM_BOARD_PLATFORMS := $(PRODUCT_PLATFORM)
 
 # Recovery
-BOARD_INCLUDE_RECOVERY_DTBO := true
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
 TARGET_VENDOR_PROP := $(DEVICE_PATH)/vendor.prop
-
-# Verified Boot
-BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # Security Patch
 PLATFORM_SECURITY_PATCH := 2099-12-31
@@ -127,41 +132,23 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
-TW_INCLUDE_REPACKTOOLS := true
 TW_NO_FLASH_CURRENT_TWRP := true # For A/B devices that has dedicated recovery, removing that option in Advance.
 TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_APEX := true
 TW_HAS_MTP := true
 TW_FORCE_KEYMASTER_VER := true
 TW_Y_OFFSET := 60
 TW_H_OFFSET := -60
 TW_FRAMERATE := 60
+TW_OVERRIDE_SYSTEM_PROPS := \
+    "ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
+# Workaround
+TARGET_COPY_OUT_VENDOR := vendor
 
 # Maintainer/Version
-include $(DEVICE_PATH)/version.mk
-
-# tell update_engine to not change dynamic partition table during updates
-# needed since our qti_dynamic_partitions does not include
-# vendor and odm and we also dont want to AB update them
-TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
-
-# Slightly overprovision dynamic partitions with 50MiB to
-# allow on-device file editing
-BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 52428800
-BOARD_VENDORIMAGE_PARTITION_RESERVED_SIZE := 52428800
-BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 52428800
-
-#super
-#PRODUCT_USE_DYNAMIC_PARTITIONS := true
-#PRODUCT_BUILD_SUPER_PARTITION := true
-
-ENABLE_CPUSETS := true
-ENABLE_SCHEDBOOST := true
-TARGET_USES_UEFI := true
-
-# Partitions (listed in the file) to be wiped under recovery.
-TARGET_RECOVERY_WIPE := $(DEVICE_PATH)/recovery.wipe
-
-# Extras
-BOARD_PROVIDES_GPTUTILS := true
-BOARD_SUPPRESS_SECURE_ERASE := true
-TW_USE_LEDS_HAPTICS := true
+TW_DEVICE_VERSION := Nokia 5.3
