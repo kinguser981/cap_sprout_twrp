1cbf2efb160da9f53b188f4eeac0e403c0190bb6
Samuel Kendall <kinguser981@gmail.com>
2025-01-30 00:13:42 +0330

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,6 +7,12 @@
 
 LOCAL_PATH := device/nokia/cap_sprout
 
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := current
+
+# API level
+PRODUCT_SHIPPING_API_LEVEL := 34
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
